a897813d825f3080dbeb63c1a193560227e231cf
JR Ladd <johnrobertladd@gmail.com>
2016-04-18 19:20:44 -0500

    getting rid of extra functions

--- prose2poem.py
+++ prose2poem.py
@@ -3,7 +3,7 @@
 import pronouncing as pr
 import sys, glob
 from textblob import TextBlob as tb
-from random import choice
+from random import choice, sample
 
 files = glob.glob(sys.argv[1]+"*.txt")
 
@@ -14,55 +14,20 @@ for file in files:
         blob = tb(text)
         blobs.append(blob)
 
-def get_stresses(wordlist):
-    stresses = []
-    for word in wordlist:
-        stress = pr.stresses_for_word(word)
-        if len(stress) > 0:
-            stress = stress[0]
-            stresses.append(stress)
-    stresses = ''.join(stresses)
-    return stresses
+shorts = [s for s in blobs[0].sentences if len(s.words) <= 10]
 
-def is_iambic(stress):
-    if len(stress) == 10 and lev.distance(str(stress), "0101010101") <= 5:
-        return True
-    else:
-        return False
+def syllable_counts(sentence):
+    count = 0
+    for word in sentence.words:
+        p = pr.phones_for_word(word)
+        if len(p) > 0:
+            sc = pr.syllable_count(p[0])
+            count += sc
+    return count
 
-def find_iambic(stress):
-    if "0101010101" in stress:
-        return True
 
-# for s in blobs[0].ngrams(n=10):
-#     stress = get_stresses(s)
-#     if find_iambic(stress) == True:
-#         print s, stress
 
-for s in blobs[0].sentences:
-    if len(s.words) <= 10:
-        print s
-# def create_dict(blob):
-#     stress_dict = {}
-#     for w in blob.tags:
-#         stress = pr.stresses_for_word(w[0])
-#         if len(stress) > 1:
-#             stress = str(stress[0])
-#         else:
-#             stress = str(stress)
-#         if stress not in stress_dict:
-#             stress_dict[stress] = [w]
-#         else:
-#             stress_dict[stress].append(w)
-#     return stress_dict
-#
-# def make_line(sd):
-#     line = [choice(sd['0']), choice(sd['10']), choice(sd['1']), choice(sd['010']), choice(sd['10']), choice(sd['1'])]
-#     return line
-#
-# stress_dict = create_dict(blobs[0])
-#
-# count = 0
-# while count <= 14:
-#     count += 1
-#     print make_line(stress_dict)
+poem = sample(shorts, 10)
+
+for p in poem:
+    print p, syllable_counts(p)
