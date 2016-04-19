da697941f7f5fe78ae96aec30c0537d8bdad5fb7
JR Ladd <johnrobertladd@gmail.com>
2016-04-19 08:10:43 -0500

    seven syllable result

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a quick experiment in poetry generation with one of the test corpora available in [Alan Liu's DH Toychest](http://dhresourcesforprojectbuilding.pbworks.com/w/page/69244469/Data%20Collections%20and%20Datasets). It was created for a meeting of the Digital Approaches Reading Group at WashU on 19 April 2016.
 
-I make use of a few NLP-related Python tools including [TextBlob](https://textblob.readthedocs.org/en/dev/index.html) and Allison Parrish's [pronouncing](https://pronouncing.readthedocs.org/en/latest/index.html).
+I make use of a few NLP-related Python tools including [TextBlob](https://textblob.readthedocs.org/en/dev/index.html) and Allison Parrish's [pronouncing](https://pronouncing.readthedocs.org/en/latest/index.html). The tool creates [syllabic verse](https://en.wikipedia.org/wiki/Syllabic_verse) rather than the more typical accented-syllabic style in English (think iambic pentameter).
+
+Syllabic verse was used by 20th-century poets including Marianne Moore, Dylan Thomas, and Thomas Nashe. It is therefore quite a bit distant from the more verbose style of 1880s prose writing for children. But the different number of short sentences in each text (as reflected in the relative lengths of the poems) calls attention to the styles of the different novels, and often this small snapshot gives insight into the tone, characters, mood, and even plot of each text.
--- prose2poem.py
+++ prose2poem.py
@@ -1,33 +1,43 @@
 #! /usr/bin/env python
 
 import pronouncing as pr
-import sys, glob
+import sys, glob, codecs
 from textblob import TextBlob as tb
-from random import choice, sample
 
 files = glob.glob(sys.argv[1]+"*.txt")
-
-blobs = []
-for file in files:
-    with open(file, 'r') as f:
-        text = f.read()
-        blob = tb(text)
-        blobs.append(blob)
-
-shorts = [s for s in blobs[0].sentences if len(s.words) <= 10]
 
 def syllable_counts(sentence):
     count = 0
     for word in sentence.words:
+        word = word.lower()
         p = pr.phones_for_word(word)
         if len(p) > 0:
             sc = pr.syllable_count(p[0])
             count += sc
     return count
 
+def create_dict(shorts):
+    syl_dict = {}
+    for sent in shorts:
+        count = syllable_counts(sent)
+        if count not in syl_dict:
+            syl_dict[count] = [sent]
+        else:
+            syl_dict[count].append(sent)
+    return syl_dict
 
-
-poem = sample(shorts, 10)
-
-for p in poem:
-    print p, syllable_counts(p)
+count = 0
+for file in files:
+    count += 1
+    with codecs.open(file, 'r', 'utf-8', errors='ignore') as f:
+        text = f.read()
+    blob = tb(text)
+    shorts = [s for s in blob.sentences if len(s.words) <= 10]
+    syl_dict = create_dict(shorts)
+    poem = [str(s)+'\n' for s in syl_dict[7]]
+    with open('seven_syllables.md', 'a') as wf:
+        wf.write('## '+file[6:]+'\n')
+        wf.write('\n')
+        wf.writelines(poem)
+        wf.write('\n')
+    print 'Wrote poem '+str(count)+'!'
